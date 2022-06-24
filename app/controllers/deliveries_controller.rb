a6da5f36bb3249281ed8d23af4066aa76c54fb76
KobayashiHanaka <konahana8787@gmail.com>
2022-06-24 08:23:31 +0000

    add deliveries index and edit

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,21 +1,33 @@
 class Public::DeliveriesController < ApplicationController
-  def index
-  end
 
-  def edit
+  def index
+    @delivery = Delivery.new(delivery_params)
+    @deliveries = Delivery.all
   end
 
   def create
+    @delivery = Delivery.new(delivery_params)
+    @delivery.customer_id = current_customer.id
+    @delivery.save
+    redirect_to deliveries_path
+  end
 
-
+  def edit
+    @delivery = Delivery.find(params[:id])
   end
 
   def update
-
+    @delivery = Delivery.find(params[:id])
+    @delivery.update(delivery_params)
+    redirect_to deliveries_path
   end
 
   def destroy
 
+  end
+
+  def delivery_params
+    params.permit(:delivery).require(:post_code,:address,:name)
   end
 
 
