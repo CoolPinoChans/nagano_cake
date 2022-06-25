2ceed2983b3b948ea83745715540086f5cb67785
EC2 Default User <ec2-user@ip-172-31-26-83.ec2.internal>
2022-06-25 10:53:07 +0000

    add admin/customer/index fix genre item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   has_one_attached :image
   has_many :carts
   has_many :order_details
-  #belongs_to :genres
+  belongs_to :genre
 
   def with_tax_price
       (price_no_tax * 1.1).floor
