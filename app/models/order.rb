ee6c0beee7dce73970f8aa1785ec63ec40cee148
tottoko1234 <bfia78j@gmail.com>
2022-06-26 07:00:01 +0000

    orders_new

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,13 @@ class Order < ApplicationRecord
     has_many :order_detail
     
     enum payment_method: { credit_card: 0, transfer: 1 }
+    enum select_address: { my_address: 0, address_index: 1, address_new: 2}
+    
+    def subtotal_check
+        item.with_tax_price * quantity
+    end
+    
+    def postage
+        800
+    end
 end
