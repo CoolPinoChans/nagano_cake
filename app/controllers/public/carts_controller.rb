40faece7c2c1cbdf8a8757c4156eb6ba50fc8727
tottoko1234 <bfia78j@gmail.com>
2022-06-26 14:23:04 +0000

    orders_confirm and fix cart_controller

--- app/controllers/public/carts_controller.rb
+++ app/controllers/public/carts_controller.rb
@@ -26,10 +26,10 @@ class Public::CartsController < ApplicationController
   def create
     @cart_item = Cart.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
-    @cart_items = current_customer.carts.find_by(item_id: params[:item_id])
+    @cart_items = current_customer.carts.find_by(item_id: params[:cart][:item_id])
     
     if @cart_items.present?
-      new_amount = @cart_items.amount + @cart_item.amount
+      new_amount = @cart_items.quantity + @cart_item.quantity
       @cart_items.update_attribute(:quantity, new_amount)
       @cart_item.delete
       redirect_to carts_path
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,11 +5,4 @@ class Order < ApplicationRecord
     enum payment_method: { credit_card: 0, transfer: 1 }
     enum select_address: { my_address: 0, address_index: 1, address_new: 2}
     
-    def subtotal_check
-        item.with_tax_price * quantity
-    end
-    
-    def postage
-        800
-    end
 end
